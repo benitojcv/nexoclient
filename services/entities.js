a38941548b79bda340978a4eb2171cbf7c684e8d
Benito <benitojcv@gmail.com>
2016-07-25 08:01:44 +0100

    Refactorizacion

--- services/entities.js
+++ services/entities.js
@@ -24,29 +24,37 @@ module.exports = {
         url: ORION_URL + '/entities',
         method: 'POST',
         json: {
-              "id": user,
-              "type": "User",
-              "event_pause": {
-                "value": 0,
-                "type": "Float",
-                "metadata": {
-                  "modifiedAt": {
-                    "value": "2016-07-21T12:56:56.00Z",
-                    "type": "DateTime"
+                "id": user,
+                "type": "User",
+                "status": {
+                  "value": "none",
+                  "type": "String"
+                },
+                "location": {
+                  "value": "0,0",
+                  "type": "geo:point"
+                },
+                "event_pause": {
+                  "value": 0,
+                  "type": "Float",
+                  "metadata": {
+                    "modifiedAt": {
+                      "value": "2016-07-21T12:56:56.00Z",
+                      "type": "DateTime"
+                    }
                   }
-                }
-              },
-              "event_play": {
-                "value": 0,
-                "type": "Float",
-                "metadata": {
-                  "modifiedAt": {
-                    "value": "2016-07-21T12:56:56.00Z",
-                    "type": "DateTime"
+                },
+                "event_play": {
+                  "value": 0,
+                  "type": "Float",
+                  "metadata": {
+                    "modifiedAt": {
+                      "value": "2016-07-21T12:56:56.00Z",
+                      "type": "DateTime"
+                    }
                   }
                 }
               }
-            }
       }, function(error, request, body){
         if (error) {
           console.log(user + " creation failed!!!");
