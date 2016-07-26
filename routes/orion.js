45c1f061f17bc475087fa6c395b4d10b816c88a4
Benito <benitojcv@gmail.com>
2016-07-26 14:46:21 +0100

    Limpieza

--- routes/orion.js
+++ routes/orion.js
@@ -4,9 +4,6 @@ var moment = require('moment');
 var entities = require('../services/entities');
 var router = express.Router();
 
-var ORION_URL = 'http://localhost:1026/v2';
-
-
 router.get('/entities/:user', function(req, res, next) {
   console.log('OK GET ' + req.params.user);
   res.send({'OK2 ' : req.params.user});
@@ -32,27 +29,6 @@ router.put('/entities/:user/attrs/:attribute', function(req, res, next) {
       res.status(500).send("ERROR: Could not create entity " + req.params.user);
     });
   });
-
-  // request({
-  //   url: ORION_URL + '/entities/' + req.params.user + '/attrs/' + req.params.attribute,
-  //   method: 'PUT',
-  //   json: {'value':0,'type':'Float','metadata':{'modifiedAt':{'value':moment().format(),'type':'DateTime'}}}
-  // }, function(error, request, body){
-  //   if (request.statusCode == 404) {
-  //     entities.createUser(req.params.user).then(function(req2) {
-  //       entities.updateUserAttribute(req.params.user, req.params.attribute).then(function(req3) {
-  //         res.status(req3.statusCode).send();
-  //       }).catch(function(error) {
-  //         res.status(500).send("ERROR: Could not update entity " + req.params.user);
-  //       });
-  //     }).catch(function(error) {
-  //       res.status(500).send("ERROR: Could not create entity " + req.params.user);
-  //     });
-  //   }
-  //   else {
-  //     res.status(request.statusCode).send(body);
-  //   }
-  // });
 });
 
 module.exports = router;
